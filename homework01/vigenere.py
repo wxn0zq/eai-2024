d8a6a2d4a90bf43dbcb6614ab274be9332bbcf24
wxn0zq <vikazyeva2017@gmail.com>
2024-10-02 23:56:26 +0300

    Реализованы шифры и проверены юнит тесты для них

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,5 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+def encrypt_vigenere(plaintext, keyword):
     """
-    Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,15 +7,24 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    ciphertext = ''
+    key_repeated = (keyword * (len(plaintext) // len(keyword))) + keyword[:len(plaintext) % len(keyword)]
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            shift = ord(key_repeated[i].upper()) - ord('A')
+            if plaintext[i].isupper():
+                ciphertext += chr((ord(plaintext[i]) + shift - ord('A')) % 26 + ord('A'))
+            else:
+                ciphertext += chr((ord(plaintext[i]) + shift - ord('a')) % 26 + ord('a'))
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
 
+def decrypt_vigenere(ciphertext, keyword):
+    """
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -25,6 +32,15 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = ''
+    key_repeated = (keyword * (len(ciphertext) // len(keyword))) + keyword[:len(ciphertext) % len(keyword)]
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            shift = ord(key_repeated[i].upper()) - ord('A')
+            if ciphertext[i].isupper():
+                plaintext += chr((ord(ciphertext[i]) - shift - ord('A')) % 26 + ord('A'))
+            else:
+                plaintext += chr((ord(ciphertext[i]) - shift - ord('a')) % 26 + ord('a'))
+        else:
+            plaintext += ciphertext[i]
     return plaintext
